c96017595c22fe3a18859a1bc07e950c37c42224
fenugrec <fenugrec@users.sourceforge.net>
2018-04-26 00:19:52 -0400

    sigrok PD : add data annot, split in rows

--- sigrok_PD/mcs48/pd.py
+++ sigrok_PD/mcs48/pd.py
@@ -56,11 +56,18 @@ class Decoder(srd.Decoder):
         ('data', 'Data'),
     )
 
+    annotation_rows = (
+        ('addr', 'Address', (0,)),
+        ('data', 'Data', (1,)),
+    )
+
     def __init__(self):
         self.addr = 0
+        self.addr_s = 0
         self.data = 0
+        self.data_s = 0
 
-        #flag to make sure we get a falling edge first
+        #flag to make sure we get an ALE pulse first
         self.started = 0
 
     def start(self):
@@ -80,7 +87,7 @@ class Decoder(srd.Decoder):
 
     def ale_rise(self):
         if self.started:
-            self.put(self.addr_s, self.samplenum, self.out_ann, [1, ['Addr: 0x%08X' % self.addr]])
+            self.put(self.addr_s, self.samplenum, self.out_ann, [0, ['A 0x%04X' % self.addr]])
 
     def newdata(self, pins):
         # falling edge on PSEN : get data
@@ -89,13 +96,18 @@ class Decoder(srd.Decoder):
         for i in range(8):
             tempdata |= pins[i] << i
         self.data = tempdata
+        self.data_s = self.samplenum
+
+    def psen_rise(self):
+        if self.started:
+            self.put(self.data_s, self.samplenum, self.out_ann, [1, ['D 0x%02X' % self.data]])
 
     def decode(self):
         # Sample address on the falling ALE edge;
         # Save data on falling edge of PSEN.
 
         while True:
-            pins = self.wait([{14: 'f'}, {14: 'r'}, {15: 'f'}])
+            pins = self.wait([{13: 'f'}, {13: 'r'}, {14: 'f'}, {14: 'r'}])
 
             # Handle those conditions (one or more) that matched this time.
             if self.matched[0]:
@@ -104,3 +116,5 @@ class Decoder(srd.Decoder):
                 self.ale_rise()
             if self.matched[2]:
                 self.newdata(pins[0:])
+            if self.matched[3]:
+                self.psen_rise()
